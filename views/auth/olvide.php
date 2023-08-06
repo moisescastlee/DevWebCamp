e63c997502955b2eb046304abb7a0a0d2253d4a0
Israel <israel@Israels-MacBook-Pro.local>
2023-08-06 13:25:37 -0400

    domingo

--- views/auth/olvide.php
+++ views/auth/olvide.php
@@ -1,6 +1,6 @@
 <main class="auth">
     <h2 class="auth__heading"><?php echo $titulo; ?></h2>
-    <p class="auth__texto">Recuperar contraseñ</p>
+    <p class="auth__texto">Recuperar tu acceso con WebCam</p>
 
     <form class="formulario" action="">
        
@@ -15,11 +15,11 @@
             >
         </div>
 
-        <input type="submit" class="formulario__submit" value="Iniciar Sesión">
+        <input type="submit" class="formulario__submit" value="Enviar instrucciones">
 
         <div class="acciones">
             <a href="/registro" class="acciones__enlace">¿Aun no tienes una cuenta?</a>
-            <a href="/olvide" class="acciones__enlace">¿Olvidaste tu password?</a>
+            <a href="/login" class="acciones__enlace">Ya tienes cuenta? Inicia Sesion!</a>
         </div>
     </form>
 </main>
---
+++ views/templates/header.php
@@ -0,0 +1,27 @@
+<header class="header">
+    <div class="header__contenedor">
+        <nav class="header__navegacion">
+            <a href="/registro" class="header__enlace"> Registro</a>
+            <a href="/login" class="header__enlace"> Iniciar session</a>
+
+            </nav>
+
+            <div class="header__contenido">
+                <a href="/">
+                    <h1 class="header__logo">
+                    &#60;DevWebCam />
+                    </h1>
+                </a>
+                
+
+
+                <p class="header__texto">Octubre 5-6 - 2 O 2 3</p>
+
+                <p class="header__texto header__texto--modalidad">En linea - Presencial</p>
+
+                <a href="/registro" class="header__boton">Comprar Pase</a>
+
+                
+        </div>
+    </div>
+</header>

1cf7566ac4e2dcb22442c625cbaf33729d86be03
Israel <israel@Israels-MacBook-Pro.local>
2023-08-07 10:12:41 -0400

    lunes

--- views/templates/header.php
+++ views/templates/header.php
@@ -5,23 +5,17 @@
             <a href="/login" class="header__enlace"> Iniciar session</a>
 
         </nav>
-
             <div class="header__contenido">
                 <a href="/">
                     <h1 class="header__logo">
                     &#60;DevWebCam />
                     </h1>
                 </a>
-                
 
-
-                <p class="header__texto">Octubre 5-6 - 2 O 2 3</p>
-
-                <p class="header__texto header__texto--modalidad">En linea - Presencial</p>
-
-                <a href="/registro" class="header__boton">Comprar Pase</a>
-
-                
-        </div>
+            <p class="header__texto">Octubre 5-6 - 2 O 2 3</p>
+            <p class="header__texto header__texto--modalidad">En linea - Presencial</p>
+            
+            <a href="/registro" class="header__boton">Comprar Pase</a>
+         </div>
     </div>
 </header>

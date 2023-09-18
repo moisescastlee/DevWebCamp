0a08875d398e0018aaca428889abfdc8b97ba0c7
Israel <israel@Israels-MacBook-Pro.local>
2023-09-18 19:56:37 -0400

    lunes

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -11,12 +11,22 @@ use Intervention\Image\ImageManagerStatic as Image;
 class PonentesController {
 
     public static function index(Router $router){
-        $pagina_actual = 1;
+
+        $pagina_actual = $_GET['page'];
+        $pagina_actual = filter_var($pagina_actual, FILTER_VALIDATE_INT);
+
+        if(!$pagina_actual || $pagina_actual < 1) {
+            header('Location: /admin/ponentes?page=1');
+        }
+
+        
         $registros_por_pagina = 10;
         $total = 100;
 
         $paginacion = new Paginacion($pagina_actual, $registros_por_pagina, $total);
+
         debuguear($paginacion);
+        
         $ponentes = Ponente::all();
 
         if(is_admin()) {

3aef5ad1319e79d09f1a6078bd0ee0bd07f54997
Israel <el_moises_05@hotmail.com>
2024-01-21 20:32:21 -0400

    domingo

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,9 +1,17 @@
 import Swiper from 'swiper';
+import 'swiper/css';
 
 document.addEventListener('DOMContentLoaded', function() {
     if(document.querySelector('.slider')) {
         const opciones = {
-            
+            slidesPerView: 3,
+            spaceBetween: 15,
+            freemode: true,
+            breakpoints: {
+                640: {
+                    slidesPerView: 1
+                }
+            }
         }
 
         new Swiper('.slider', opciones)

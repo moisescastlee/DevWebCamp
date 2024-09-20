eb6b807238dce509d0acfbed39d1ff082a1e06a3
moisescastlee <el_moises_05@hotmail.com>
2024-09-20 16:50:11 -0400

    vierrnes

--- controllers/RegistroController.php
+++ controllers/RegistroController.php
@@ -2,10 +2,15 @@
 
 namespace Controllers;
 
-use Model\Paquete;
+use Model\Dia;
+use Model\Hora;
 use MVC\Router;
-use Model\Registro;
+use Model\Evento;
+use Model\Paquete;
+use Model\Ponente;
 use Model\Usuario;
+use Model\Registro;
+use Model\Categoria;
 
 class RegistroController {
 
@@ -135,11 +140,37 @@ class RegistroController {
             header('Location: /');
         }
 
+        $eventos = Evento::ordenar('hora_id', 'ASC');
+        $eventos_formateados = [];
+        foreach($eventos as $evento) {
+
+            $evento->categoria = Categoria::find($evento->categoria_id);
+            $evento->dia = Dia::find($evento->dia_id);
+            $evento->ponente = Ponente::find($evento->ponente_id);
+            $evento->hora = Hora::find($evento->hora_id);
+
+            if($evento->dia_id === "1" && $evento->categoria_id === "1"){
+                $eventos_formateados['conferencias_v'][] = $evento;
+            }
+
+            if($evento->dia_id === "2" && $evento->categoria_id === "1"){
+                $eventos_formateados['conferencias_s'][] = $evento;
+            }
+
+            if($evento->dia_id === "1" && $evento->categoria_id === "2"){
+                $eventos_formateados['workshops_v'][] = $evento;
+            }
+
+            if($evento->dia_id === "2" && $evento->categoria_id === "2"){
+                $eventos_formateados['workshops_s'][] = $evento;
+            }
+        }
+
 
 
         $router->render('registro/conferencias', [
             'titulo' => 'Elige Workshops y Conferencias',
-            
+            'eventos' => $eventos_formateados
 
         ]);
     }
--- views/registro/conferencias.php
+++ views/registro/conferencias.php
@@ -1,5 +1,20 @@
-<main class="pagina">
+
     <h2 class="pagina__heading"><?php echo $titulo; ?></h2>
     <p class="pagina__descripcion"> Elige hasta 5 eventos para asistir presencial. </p>
 
-<main/>
+<div class="eventos-registro"> 
+    <main class="eventos-registros__listado">
+        <h3 class="eventos-registro__heading--conferencias">&lt;Conferencias /></h3>
+        <p class="eventos__fecha">Jueves 31 de Octubre</p>
+
+        <div class="eventos-registro__grid">
+            <?php foreach($eventos['conferencias_v'] as $evento) { ?>
+                <?php include __DIR__ . '/evento.php'; ?>
+            <?php } ?>
+        </div>
+    </main>
+
+    <aside class="registro">
+                <h2 class="registro__heading"> Tu registro </h2>
+    </aside>
+</div>
